097006bc21435260b25a18ba6c93fa5c51a4dea9
André Prodinger <prodinger.andre@gmail.com>
2021-11-11 11:29:17 +0100

    Server and Client is connected
    + Server Http requests works
    + Client can download Files from server, yow

--- Server.py
+++ Server.py
@@ -1,31 +1,21 @@
 from aiohttp import web
-import asyncio
-from aiohttp_session import setup, get_session
-from asyncio.base_events import _ipaddr_info
-import requests
-
-def upload_file():
-    test_file = open ('my_file.txt', 'rb')
-    test_url = "http://localhost:8080"
-    test_response = requests.post(test_url, files = {"form_field_name": test_file})
-    if test_response.ok:
-        print("Upload completed successfully!")
-        print(test_response.text)
-    else:
-        print("Something went wrong!")
 
 async def handle(request):
-    name = request.match_info.get('name', 'Virus')
+    name = request.match_info.get('name')
     print('Connection created: ' + request.remote)
-    return web.Response(text=name)
+    try:
+        file = open(name, 'r')
+        content = file.read()
+        file.close()
+        return web.Response(text=content)
+    except FileNotFoundError:
+        return 'Sorry file not here lol'
 
 app = web.Application()
-app.add_routes([web.get('/', handle),
-                web.get('/{name}', handle)])
+app.add_routes([web.get('/{name}', handle)])
 
 if __name__ == '__main__':
-    web.run_app(app)
-    upload_file()  
+    web.run_app(app) 
     
 
 
--- client.py
+++ client.py
@@ -1,24 +1,19 @@
+from http.client import EXPECTATION_FAILED
 import aiohttp
 import asyncio
-import requests
+import urllib.request
+import time
+import subprocess
 
-
-url = 'https://localhost:8080'
+def download():
+    nurl = 'http://localhost:8080/script2.py'
+    urllib.request.urlretrieve(nurl, 'script2.py')
 
 async def main():
-
-
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            
-            r = requests.get(url, allow_redirects=True)
-            open('my_file.txt', 'wb').write(r.content)
-            print("Status:", response.status)
-            print("Content-type:", response.headers['content-type'])
-
-            html = await response.text()
-            print("Body:", html[:15], "...")
+    async with aiohttp.ClientSession(trust_env=True) as session:
+        download()
+        #time.sleep(1)  
+        subprocess.Popen("script2.py 1", shell=True)
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
-
